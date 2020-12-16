291ed47be3744c5c089e6b801f0fe8c293311c3c
dhgillespie97 <38857821+dhgillespie97@users.noreply.github.com>
2020-12-16 10:45:22 -0500

    Add files via upload

---
+++ pass_gen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+class PassGen
+{
+    public static string Main(string[] args, int length)
+    {
+        var charcount = new Random();
+        var list = new List<string> { "9", "10", "11", "12", "13", "14" };
+        int index = charcount.Next(list.Count);
+        var repeat = (list[index]);
+
+        Int32.TryParse(repeat, out int val);
+        Console.WriteLine("\nThis is val: " + val);
+
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&";
+        StringBuilder res = new StringBuilder();
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] uintBuffer = new byte[sizeof(uint)];
+
+            while (length-- > 0)
+            {
+                rng.GetBytes(uintBuffer);
+                uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                res.Append(chars[(int)(num % (uint)chars.Length)]);
+
+            }
+        }
+        return res.ToString();
+    }
+}
---
+++ simple_test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nGet ready to play... Press any key when ready.");
+        Console.ReadLine();
+        var random = new Random();
+        var list = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        int index = random.Next(list.Count);
+
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        Console.WriteLine(list[index]);
+        var randomChosen = (list[index]);
+
+        string answer = Console.ReadLine();
+        if (answer == null || answer != randomChosen)
+            {
+                Console.WriteLine("\nYour answer is incorrect. Please play again.");
+            }
+        if (answer == randomChosen)
+        {
+            Console.WriteLine("Congratulations! You are not an idiot. Thanks for playing. Here is your time- \n");
+        }
+
+        TimeSpan ts = stopWatch.Elapsed;
+        string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+        Console.WriteLine("\nRunTime " + elapsedTime);
+    }
+}
